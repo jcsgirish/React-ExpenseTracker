33e784a22c5edc6978b6650f44eae27e80502b5a
jcsgirish <jcsgirish7@gmail.com>
2023-03-07 21:26:04 +0530

    React Basics

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import ExpenseItem from "./component/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h1>Expense Items !</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+
+  );
+}
+
+export default App;
