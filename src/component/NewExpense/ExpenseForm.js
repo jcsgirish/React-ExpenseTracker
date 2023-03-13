4a0de22dc429ace52b1e94d7018c27a2db8c2099
jcsgirish <jcsgirish7@gmail.com>
2023-03-14 00:37:47 +0530

    React Basics

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -2,25 +2,31 @@ import React from "react"
 import "./ExpenseForm.css"
 
 const Expenseform=()=>{
+  const clickhandle=(event)=>{
+    return (
+      console.log(event.target.value)
+    )
+
+  }
     return (
         <form>
         <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
           <input
-            type='text'
+            type='text'onChange={clickhandle}
           />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
           <input
-            type='number' min='0.01'step='0.01'
+            type='number' min='0.01'step='0.01'onChange={clickhandle}
           />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
           <input
-            type='date'min='2019-01-01'max='2022-12-31'
+            type='date'min='2019-01-01'max='2022-12-31'onChange={clickhandle}
           />
         </div>
       </div>
