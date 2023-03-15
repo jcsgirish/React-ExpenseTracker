b6f0c1d3f38ff51ae10f1b0ad1027b1339c88f3e
jcsgirish <jcsgirish7@gmail.com>
2023-03-16 01:51:25 +0530

    React Basics

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewExpense from './component/NewExpense/NewExpense';
 import Expenses from  "./component/Expenses/Expenses";
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Cricket',
-      amount: 200,
-      date: new Date(2023, 5, 4),
-    },
-    { id: 'e2',
-     title: 'Rugby',
-      amount: 800,
-       date: new Date(2023, 2, 9) },
-    {
-      id: 'e3',
-      title: 'football',
-      amount: 100,
-      date: new Date(2023, 10, 8),
-    },
-    {
-      id: 'e4',
-      title: 'Soccer',
-      amount: 500,
-      date: new Date(2023, 7, 1),
-    }
 
-  ];
+
+const DummyExpenses = [
+  {
+    id: 'e1',
+    title: 'Cricket',
+    amount: 200,
+    date: new Date(2020, 5, 4),
+  },
+  { id: 'e2',
+   title: 'Rugby',
+    amount: 800,
+     date: new Date(2021, 2, 9) },
+  {
+    id: 'e3',
+    title: 'football',
+    amount: 100,
+    date: new Date(2022, 10, 8),
+  },
+  {
+    id: 'e4',
+    title: 'Soccer',
+    amount: 500,
+    date: new Date(2023, 7, 1),
+  }
+
+];
+
+const App = () => {
+  const [expenses,setExpenses]=useState(DummyExpenses);
 
   const addexpensehandler=(expense)=>{
-    console.log(expense)
-  };
-  return (
+    setExpenses((prevExpenses)=>{
+
+      return [expense,...prevExpenses]
+      });
+    };
+return(
     <div>
  
      <NewExpense onAddingExpense={addexpensehandler}></NewExpense>
-
-
-
       <Expenses items={expenses} />
-
-
-    </div>
- 
-
-  );
+   </div>
+)
 }
 export default App;
