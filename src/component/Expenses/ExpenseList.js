600abb3417d0f895c2b7d0546bddedf47cc45347
jcsgirish <jcsgirish7@gmail.com>
2023-03-18 01:15:53 +0530

    Baisc React

--- src/component/Expenses/ExpenseList.js
+++ src/component/Expenses/ExpenseList.js
@@ -1,29 +1,22 @@
-
-
 import React from "react"
 
 import ExpenseItem from "./ExpenseItem";
 import "./ExpenseList.css"
 
 const ExpenseList = (props)=>{
-    if(props.items.length===1){
-        return <h2 className="expenses-lists">Only single Expense here. Please add more...</h2>
-    }
-
     return (
-    <ul className="expense-list">
-        {props.items.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-     ))}
-
-    </ul>
-    )
-
+        <ul >
+            {props.items.length ===1 &&  <h2 className="expenses-lists">Only single Expense here. Please add more...</h2>} 
+            {props.items.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date}
+                />
+            ))}
+        </ul>
+    );
 };
 
-export default ExpenseList  
+export default ExpenseList;
